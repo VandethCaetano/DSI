c48ebc04c6caf68649f17cfb97c205829491726e
VandethCaetano <caetanovandeth@gmail.com>
2024-10-09 12:10:52 -0300

    Add files via upload

---
+++ age06dsi/002.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+        <title>Boletim 8ª Ano</title>       
+    
+        <style>
+        
+            .w3-border-white {
+                border: 5px solid white; 
+            }
+
+            .custom-table1, .custom-table2  {
+                width: 110%;
+                text-align: center; 
+                white-space: nowrap; 
+            }
+
+        </style>
+    </head>
+
+    <body>
+        <div class="w3-third w3-center w3-animate-top w3-padding">
+
+            <div class="w3-table-all w3-hoverable w3-text-white w3-blue w3-border-white custom-table1">
+            <h2> 8ª Ano A</h2>
+        </div>
+           
+            <?php
+                $alunos = array(
+                    "Nome" => array("Aline", "Alfredo", "Carla", "César", "Daniel", "Esnar", "Henzo", "Pablo", "Wallace", "Zulmira"),
+                    "Primeiro Semestre" => array("10", "8", "5", "9", "10", "8", "6", "7", "8", "10"),
+                    "Segundo Semestre" => array("9.5", "5", "6.5", "9", "7", "6", "8", "5", "7", "6")
+                );
+                $num_alunos = count($alunos["Nome"]);
+            ?>
+
+            <table class="w3-table-all w3-hoverable w3-text-black w3-border-white custom-table2 ">
+                <tr class="w3-blue">
+                    <th class="w3-center">Nome</th>
+                    <th class="w3-center">Primeiro Semestre</th>
+                    <th class="w3-center">Segundo Semestre</th>
+                </tr>
+
+                <?php for ($i = 0; $i < $num_alunos; $i++) { ?>
+                    <tr>
+                        <td class="w3-center"><?php echo htmlspecialchars($alunos["Nome"][$i]); ?></td>
+                        <td class="w3-center"><?php echo htmlspecialchars($alunos["Primeiro Semestre"][$i]); ?></td>
+                        <td class="w3-center"><?php echo htmlspecialchars($alunos["Segundo Semestre"][$i]); ?></td>
+                    </tr>
+                <?php } ?>
+
+            </table>       
+        </div>
+    </body>
+</html>
---
+++ age06dsi/agenda06dsi.sql
@@ -0,0 +1,58 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 13/09/2024 às 15:23
+-- Versão do servidor: 10.4.32-MariaDB
+-- Versão do PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+-- Banco de dados: `agenda06dsi`
+--
+-- --------------------------------------------------------
+
+-- Estrutura para tabela `aluno_concluinte`
+--
+
+CREATE TABLE `aluno_concluinte` (
+  `id_aluno`  int auto_increment primary key,
+  `nome` varchar(45) NOT NULL,
+  `nota1` float DEFAULT NULL,
+  `nota2` float DEFAULT NULL,
+  `nota3` float DEFAULT NULL,
+  `nota4` float DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+-- Despejando dados para a tabela `aluno_concluinte`
+--
+INSERT INTO 'aluno_concluinte'('nome', 'nota1', 'nota2', 'nota3', 'nota4')
+VALUES
+('Eduarda', '10', '10', '9', '10'),
+('Paulo', '10', '9', '8', '9'),
+('Igo', '10', '8', '10', '10'),
+('Guilherme', '10', '9', '8', '7'),
+('Henrique', '9', '9', '8', '7'),
+('Nilson', '10', '9', '10', '9'),
+('Ana', '9', '9', '8', '8'),
+('Amanda', '8', '9', '9', '8'),
+('Carlos', '6', '6', '5', '6'),
+('Pedro', '8', '8', '8', '7');
+
+
+
+
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
